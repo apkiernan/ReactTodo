5fd2e455c8973b4470eea1322254d13e48aa9133
Andrew Kiernan <apkiernan@gmail.com>
2015-03-13 15:41:40 -0400

    added persistance to localStorage

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,153 +1,153 @@
-var TodoApp = React.createClass({
+(function () {
+	var TodoApp = React.createClass({
 
-	getInitialState: function () {
-		return null;
-	},
+		componentDidMount: function () {
+			this.props.store.subscribe('change', function () {
+				React.render(<App store={TodoStore} />, document.querySelector('#content'));
+			});
+		},
 
-	componentDidMount: function () {
-		this.props.store.subscribe('change', function () {
-			React.render(<App store={TodoStore} />, document.querySelector('#content'));
-		});
-	},
+		render: function () {
 
-	render: function () {
+			return (
+				<section className="todoApp">
+					<TodoForm save={this._save} />
+					<TodoList 
+						todos={this.props.store.todos}
+						complete={this._toggleComplete} />
+				</section>
+			);
+		},
 
-		return (
-			<section className="todoApp">
-				<TodoForm save={this._save} />
-				<TodoList 
-					todos={this.props.store.todos}
-					complete={this._toggleComplete} />
-			</section>
-		);
-	},
+		_save: function (todo) {
+			this.props.store.save(todo);
+		},
 
-	_save: function (todo) {
-		this.props.store.save(todo);
-	},
-
-	_toggleComplete: function (todo) {
-		this.props.store.toggleComplete(todo);
-	}
-});
-
-var TodoList = React.createClass({
-
-	toggleComplete: function (todo) {
-		this.props.complete(todo);
-	},
-
-	render: function () {
-		return (
-			<ul>
-				{this.props.todos.map(function (todo) {
-					return (
-						<Todo todo={todo} 
-									key={todo.id}
-									onChange={this.toggleComplete.bind(this, todo)} />
-					);
-				}, this)}
-			</ul>
-		);
-	}
-});
-
-var TodoForm = React.createClass({
-
-	propTypes: {
-
-	},
-
-	_handleSubmit: function (e) {
-		e.preventDefault();
-
-		var todoModel = {
-			completed: false,
-			text: this.refs.text.getDOMNode().value.trim(),
-			id: ++id
-		};
-
-		this.props.save(todoModel);
-		this.refs.text.getDOMNode().value = '';
-	},
-
-	render: function () {
-		return (
-			<form onSubmit={this._handleSubmit}>
-				<input type="text" placeholder="What do you need to do?" ref="text" />
-				<button type="submit">Save</button>
-			</form>
-		);
-	}
-});
-
-var Todo = React.createClass({
-
-	render: function () {
-
-		var completed = this.props.todo.completed;
-
-		return (
-			<li key={this.props.key}
-				className={completed ? 'delete' : ''}>
-					<input type="checkbox" onChange={this.props.onChange} />
-					<span>{this.props.todo.text}</span>
-			</li>
-		);
-	}
-});
-
-var Controls = React.createClass({
-
-	markComplete: function () {
-		return this.props.store.completeAll();
-	},
-
-	clearComplete: function () {
-		return this.props.store.clearCompleted();
-	},
-
-	clearAll: function () {
-		return this.props.store.clearAll();
-	},
-
-	_handleClick: function (e) {
-		e.preventDefault();
-
-		switch(e.target.id) {
-			case 'markComplete':
-				this.markComplete();
-				break;
-			case 'clearComplete':
-				this.clearComplete();
-				break;
-			case 'clearAll':
-				this.clearAll();
-				break;
+		_toggleComplete: function (todo) {
+			this.props.store.toggleComplete(todo);
 		}
-	},
+	});
 
-	render: function () {
-		return (
-			<ul className="footer" onClick={this._handleClick}>
-				<li><a href="#" id="markComplete">Complete All</a></li>
-				<li><a href="#" id="clearComplete">Clear Completed</a></li>
-				<li><a href="#" id="clearAll">Clear All</a></li>
-			</ul>
-		);
-	}
-});
+	var TodoList = React.createClass({
 
-var App = React.createClass({
-	render: function () {
-		return (
-			<div>
-				<TodoApp store={this.props.store}/>
-				<Controls store={this.props.store}/>
-			</div>
-		);
-	}
-});
+		toggleComplete: function (todo) {
+			this.props.complete(todo);
+		},
 
-window.onload = React.render(<App store={TodoStore}/>, document.querySelector('#content'));
+		render: function () {
+			return (
+				<ul>
+					{this.props.todos.map(function (todo) {
+						return (
+							<Todo todo={todo} 
+										key={todo.id}
+										onChange={this.toggleComplete.bind(this, todo)} />
+						);
+					}, this)}
+				</ul>
+			);
+		}
+	});
+
+	var TodoForm = React.createClass({
+
+		_handleSubmit: function (e) {
+			e.preventDefault();
+
+			if (this.refs.text.getDOMNode().value.trim()) {
+
+				var todoModel = {
+					completed: false,
+					text: this.refs.text.getDOMNode().value.trim(),
+				};
+
+				this.props.save(todoModel);
+			} 
+
+			this.refs.text.getDOMNode().value = '';
+		},
+
+		render: function () {
+			return (
+				<form onKeydown={this._verifyInput}
+							onSubmit={this._handleSubmit}>
+					<input type="text" placeholder="What do you need to do?" ref="text" />
+					<button type="submit">Save</button>
+				</form>
+			);
+		}
+	});
+
+	var Todo = React.createClass({
+
+		render: function () {
+
+			var completed = this.props.todo.completed;
+
+			return (
+				<li key={this.props.key}
+						className={completed ? 'delete' : ''}>
+						<input type="checkbox" 
+								onChange={this.props.onChange}
+								checked={completed ? 'checked' : ''} />
+						<span>{this.props.todo.text}</span>
+				</li>
+			);
+		}
+	});
+
+	var Controls = React.createClass({
+
+		markComplete: function () {
+			return this.props.store.completeAll();
+		},
+
+		clearComplete: function () {
+			return this.props.store.clearCompleted();
+		},
+
+		clearAll: function () {
+			return this.props.store.clearAll();
+		},
+
+		_handleClick: function (e) {
+			e.preventDefault();
+
+			switch(e.target.id) {
+				case 'markComplete':
+					this.markComplete();
+					break;
+				case 'clearComplete':
+					this.clearComplete();
+					break;
+				case 'clearAll':
+					this.clearAll();
+					break;
+			}
+		},
+
+		render: function () {
+			return (
+				<ul className="footer" onClick={this._handleClick}>
+					<li><span id="markComplete">Complete All</span></li>
+					<li><span id="clearComplete">Clear Completed</span></li>
+					<li><span id="clearAll">Clear All</span></li>
+				</ul>
+			);
+		}
+	});
+
+	var App = React.createClass({
+		render: function () {
+			return (
+				<div>
+					<TodoApp store={this.props.store}/>
+					<Controls store={this.props.store}/>
+				</div>
+			);
+		}
+	});
+
+	window.onload = React.render(<App store={TodoStore}/>, document.querySelector('#content'));
+})();
 
--- app/scripts/todoStore.js
+++ app/scripts/todoStore.js
@@ -1,57 +1,72 @@
-var id = 0;
+(function () {
 
-var TodoStore = {
-	subscribers: {},
-	todos: [
-		{completed: false, text: 'Learn React', id: ++id}
-	]
-};
+	var getId = function () {
+		return Date.now() * Math.floor(Math.random() * 7);
+	};
 
-TodoStore.prototype = Object.prototype;
+	var TodoStore = {
+		subscribers: {},
+		todos: (function () {
+			if (localStorage.getItem('react-todo')) {
+				return JSON.parse(localStorage.getItem('react-todo'));
+			} else {
+				return [];
+			}
+		})()
+	};
 
-TodoStore.prototype.publish = function (event) {
-	if (!this.subscribers[event]) {
-		return;
-	}
-	for (var i = 0; i < this.subscribers[event].length; i++) {
-		this.subscribers[event][i]();
-	}
-};
+	TodoStore.prototype = Object.prototype;
 
-TodoStore.prototype.subscribe = function (ev, func) {
-	if (this.subscribers[ev]) {
-		this.subscribers[ev].push(ev)
-	} else {
-		this.subscribers[ev] = [func];
-	}
-};
+	TodoStore.prototype.publish = function (event) {
+		if (!this.subscribers[event]) {
+			return;
+		}
+		for (var i = 0; i < this.subscribers[event].length; i++) {
+			this.subscribers[event][i]();
+		}
+	};
 
+	TodoStore.prototype.subscribe = function (ev, func) {
+		if (this.subscribers[ev]) {
+			this.subscribers[ev].push(func)
+		} else {
+			this.subscribers[ev] = [func];
+		}
+	};
 
-TodoStore.prototype.save = function (todo) {
-	this.todos.push(todo);
-	this.publish('change');
-};
+	TodoStore.prototype.save = function (todo) {
+		todo.id = getId();
+		this.todos.push(todo);
+		this.publish('change');
+	};
 
-TodoStore.prototype.toggleComplete = function (todo) {
-	todo.completed = !todo.completed;
-	this.publish('change');
-};
+	TodoStore.prototype.toggleComplete = function (todo) {
+		todo.completed = !todo.completed;
+		this.publish('change');
+	};
 
-TodoStore.prototype.completeAll = function () {
-	this.todos.map(function (todo) {
-		return todo.completed = true;
+	TodoStore.prototype.completeAll = function () {
+		this.todos.map(function (todo) {
+			return todo.completed = true;
+		});
+		this.publish('change');
+	};
+
+	TodoStore.prototype.clearCompleted = function () {
+		this.todos = this.todos.filter(function (todo) {
+			return !todo.completed;
+		}, this);
+		this.publish('change');
+	};
+
+	TodoStore.prototype.clearAll = function () {
+		this.todos = [];
+		this.publish('change');
+	};
+
+	TodoStore.subscribe('change', function () {
+		localStorage.setItem('react-todo', JSON.stringify(TodoStore.todos));
 	});
-	this.publish('change');
-};
 
-TodoStore.prototype.clearCompleted = function () {
-	this.todos = this.todos.filter(function (todo) {
-		return !todo.completed;
-	}, this);
-	this.publish('change');
-};
-
-TodoStore.prototype.clearAll = function () {
-	this.todos = [];
-	this.publish('change');
-};
+	window.TodoStore = TodoStore;
+})();
