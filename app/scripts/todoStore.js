01ba205053b9e7c8f60afc05c6cfdd7c2b79e6eb
Andrew Kiernan <apkiernan@gmail.com>
2015-03-29 15:59:19 -0400

    Change TodoStore to use Object.create.

--- app/scripts/todoStore.js
+++ app/scripts/todoStore.js
@@ -4,7 +4,7 @@
 		return Date.now() * Math.floor(Math.random() * 7);
 	};
 
-	var TodoStore = {
+	var TodoStore = Object.create({
 		subscribers: {},
 		todos: (function () {
 			if (localStorage.getItem('react-todo')) {
@@ -12,57 +12,55 @@
 			} else {
 				return [];
 			}
-		})()
-	};
+		})(),
+		
+		publish: function (event) {
+			if (!this.subscribers[event]) {
+				return;
+			}
+			for (var i = 0; i < this.subscribers[event].length; i++) {
+				this.subscribers[event][i]();
+			}
+		},
 
-	TodoStore.prototype = Object.prototype;
+		subscribe: function (ev, func) {
+			if (this.subscribers[ev]) {
+				this.subscribers[ev].push(func)
+			} else {
+				this.subscribers[ev] = [func];
+			}
+		},
 
-	TodoStore.prototype.publish = function (event) {
-		if (!this.subscribers[event]) {
-			return;
+		save: function (todo) {
+			todo.id = getId();
+			this.todos.push(todo);
+			this.publish('change');
+		},
+
+		toggleComplete: function (todo) {
+			todo.completed = !todo.completed;
+			this.publish('change');
+		},
+
+		completeAll: function () {
+			this.todos.map(function (todo) {
+				return todo.completed = true;
+			});
+			this.publish('change');
+		},
+
+		clearCompleted: function () {
+			this.todos = this.todos.filter(function (todo) {
+				return !todo.completed;
+			}, this);
+			this.publish('change');
+		},
+
+		clearAll: function () {
+			this.todos = [];
+			this.publish('change');
 		}
-		for (var i = 0; i < this.subscribers[event].length; i++) {
-			this.subscribers[event][i]();
-		}
-	};
-
-	TodoStore.prototype.subscribe = function (ev, func) {
-		if (this.subscribers[ev]) {
-			this.subscribers[ev].push(func)
-		} else {
-			this.subscribers[ev] = [func];
-		}
-	};
-
-	TodoStore.prototype.save = function (todo) {
-		todo.id = getId();
-		this.todos.push(todo);
-		this.publish('change');
-	};
-
-	TodoStore.prototype.toggleComplete = function (todo) {
-		todo.completed = !todo.completed;
-		this.publish('change');
-	};
-
-	TodoStore.prototype.completeAll = function () {
-		this.todos.map(function (todo) {
-			return todo.completed = true;
-		});
-		this.publish('change');
-	};
-
-	TodoStore.prototype.clearCompleted = function () {
-		this.todos = this.todos.filter(function (todo) {
-			return !todo.completed;
-		}, this);
-		this.publish('change');
-	};
-
-	TodoStore.prototype.clearAll = function () {
-		this.todos = [];
-		this.publish('change');
-	};
+	});
 
 	TodoStore.subscribe('change', function () {
 		localStorage.setItem('react-todo', JSON.stringify(TodoStore.todos));
